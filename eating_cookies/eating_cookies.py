5c7741f1e58738b0325a69f07d2387d77856a94e
Roger McConkie <roger.mcconkiejr@gmail.com>
2020-01-23 15:19:27 -0700

    eating cookies working

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,16 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  #Add all the n -1 combinations with 1 in front
+  #Add all the n-2 combinations with a 2 in front
+  #Add all hte n-3 combinations with a 3 in front
+  if cache is None or type(cache) == list:
+    cache = {0: 1, 1: 1, 2: 2}
+  if n < 0:
+    return 0
+  elif n not in cache:
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
